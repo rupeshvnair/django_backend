eb5bea285dc68be5768ef77e9fce3bbd021da821
rupeshvn <codewithrupeshv@gmail.com>
2025-08-04 09:20:57 +0530

    asdfasdf

--- NewFrontend/settings.py
+++ NewFrontend/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'NewFrontend.urls'
 
