b7783aa8615e36436e1b700bf74596dc8c439c1f
rupeshvn <codewithrupeshv@gmail.com>
2025-08-04 09:11:44 +0530

    asdfasd

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import JewelryItemViewSet
-
+from django.urls import path
+from .views import cors_test_view
 
 router = DefaultRouter()
 router.register(r'jewelry',JewelryItemViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
-]
-
-from django.urls import path
-from .views import cors_test_view
-
-urlpatterns = [
     path("cors-test/", cors_test_view),
 ]
